7b4553db6cd0ea86b5adfd0c5db66c4e8e30fcb1
A Fakhrurozi S <haifahrul@gmail.com>
2021-06-28 00:13:02 +0700

    Fix lint

--- src/components/FilterPanel/Checkbox/Checkbox.tsx
+++ src/components/FilterPanel/Checkbox/Checkbox.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import * as React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core';
 import { ICheckbox, ICheckboxProps } from './interfaces';
